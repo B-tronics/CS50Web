f2f3641e357e99dafe1a5348200ebe8160af125d
B-tronics <balintsupper@gmail.com>
2022-12-10 19:12:36 +0100

    feat(encyclopedia/views.py): implement search view

--- project_1/wiki/encyclopedia/views.py
+++ project_1/wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
 import markdown2
+from random import choice
 
 from . import util
 
@@ -19,7 +20,22 @@ def entry(request, title):
     else:
         return render(request, "encyclopedia/entry.html", {
             "entry": markdown2.markdown(markdown2.markdown(content))
-        })
+
+def search(request):
+    if request.method == 'POST':
+        query = request.POST['q']
+        if util.get_entry(query):
+            return entry(request, query)
+        else:
+            all_entry = util.list_entries()
+            sub_entries = util.filter_query(all_entry, query)
+            return render(request, "encyclopedia/search.html", {
+                'entries': sub_entries
+            })
+    else:
+        return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
 
 
 def page_not_found_error_handler(request, exception=None):
