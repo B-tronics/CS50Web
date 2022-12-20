794e8df801fc013102729d8114aa3023d615331e
Supper Bálint <supper.balint@ibm.com>
2022-12-20 16:04:42 +0100

    feat(project_2/commerce/auctions/models.py): add description field to the Auctions model

--- project_2/commerce/auctions/models.py
+++ project_2/commerce/auctions/models.py
@@ -6,4 +6,5 @@ class User(AbstractUser):
     pass
 
 class Auctions(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True)
