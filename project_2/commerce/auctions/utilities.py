4e0fc19215e63500d3936d2539fbb077ed08b8ff
Supper Bálint <supper.balint@ibm.com>
2022-12-21 14:54:30 +0100

    feat(project_2/commerce/auctions/utilities.py): define retrive data for index view function

--- project_2/commerce/auctions/utilities.py
+++ project_2/commerce/auctions/utilities.py
@@ -1,9 +1,12 @@
 from .models import Auctions, Bids
 
 
-def create_new_auction_record(record_data: dict):
+def create_new_auction_record(record_data: dict) -> Auctions:
     return Auctions(**record_data)
 
 
-def create_new_bid_record(record_data: dict):
+def create_new_bid_record(record_data: dict) -> Bids:
     return Bids(**record_data)
+
+def retrieve_data_for_index_view() -> dict:
+    return Auctions.objects.all()
