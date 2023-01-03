bd904fd22da79c74374366c89287050278938c6c
Supper Bálint <supper.balint@ibm.com>
2023-01-03 14:52:59 +0100

    feat(project_2/commerce/auctions/utilities.py): add bid_is_valid function

--- project_2/commerce/auctions/utilities.py
+++ project_2/commerce/auctions/utilities.py
@@ -9,7 +9,11 @@ def create_new_bid_record(record_data: dict) -> Bids:
     return Bids(**record_data)
 
 def retrieve_data_for_index_view() -> dict:
-    return Auctions.objects.all()    return Auctions.objects.all()
+    return Auctions.objects.all()
 
 def retrieve_record_count(table: object) -> int:
     return table.objects.all().count()
+
+def bid_is_valid(bid: int, auction_id: int) -> bool:
+    current_bid = Auctions.objects.get(pk=auction_id).current_bid
+    return bid > current_bid
